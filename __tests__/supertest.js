f200a97db593ff250d14b2d2b7d39c7117a2113c
Ryan Stankowitz <147885192+RyanStankowitz@users.noreply.github.com>
2023-11-29 16:20:58 -0500

    backend testsing complete

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,15 +1,42 @@
 const request = require('supertest');
 //const app = require('../server/server.js');
 
+const local3000 = 'http://localhost:3000';
+
 describe('Route integration', () => {
   describe('/', () => {
     describe('GET', () => {
       it('responds with status of 200 and content type of text/html', () => {
-        return request('http://localhost:3000')
+        return request(local3000)
           .get('/')
           .expect(200)
           .expect('Content-Type', /text\/html/);
       });
     });
   });
+});
+
+describe('trail Mix', () => {
+  describe('/', () => {
+    describe('GET', () => {
+      it('responds with status of 200 and content type of text/html', () => {
+        return request(local3000)
+          .get('/api/trails?lat=33.573244&lon=-112.245852&radius=50')
+          .expect(200)
+          .expect('Content-Type', /application\/json/);
+      }, 10000);
+    });
+  });
+});
+
+
+describe('GET /users', function() {
+  it('responds with json', async function() {
+    const response = await request(local3000)
+      .get('/api/trails?lat=33.573244&lon=-112.245852&radius=50')
+      .set('Accept', 'application/json')
+    //expect(response.headers["Content-Type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+    expect(response.body.data).toBeInstanceOf(Array)
+  }, 15000);
 });
