717ca68987b3b2e758707a0caadce3904e6bf383
kchandler44 <kellychandler624@gmail.com>
2023-11-29 10:28:09 -0700

    beginning tests complete

--- __tests__/supertest.js
+++ __tests__/supertest.js
@@ -1,33 +1,15 @@
 const request = require('supertest');
-const app = require('../server/server.js');
+//const app = require('../server/server.js');
 
-describe('GET /', () => {
-  it('responds with status of 200 and content type of text/html', () => {
-    request(app).get('/').expect(200).expect('Content-Type', 'text/html');
+describe('Route integration', () => {
+  describe('/', () => {
+    describe('GET', () => {
+      it('responds with status of 200 and content type of text/html', () => {
+        return request('http://localhost:3000')
+          .get('/')
+          .expect(200)
+          .expect('Content-Type', /text\/html/);
+      });
+    });
   });
 });
-
-//   .expect(function(res) {
-//     if (!res.body.hasOwnProperty('status')) throw new Error("Expected 'status' key!");
-//     if (!res.body.hasOwnProperty('message')) throw new Error("Expected 'message' key!");
-//   })
-//   .end(function(err, res) {
-//     if (err) throw err;
-
-//   const request = require('supertest');
-
-// describe('/', function() {
-//   it('responds with html', function(done) {
-//     request(server)
-//       .get('/')
-//       .expect('Content-Type', 'text/html')
-//       .expect(200)
-//       .end(function (err: any, _res: any) {
-//         if (err) {
-//           console.log('Error in the serving of the static html file.');
-//           return done(err);
-//         }
-//         return done();
-//       });
-//   });
-// });
